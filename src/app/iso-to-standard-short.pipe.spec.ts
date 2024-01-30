6056801da4a480c5e198fde1399708de3617879d
Ben Furner <bfurner27@yahoo.com>
2024-01-30 14:27:58 -0700

    Complete Formatting Dashboard Widgets
    Description:
    - Adjusting the dashboard card header to have a dark background instead
      of a light background
    - Adjusting the widgets to use a larger text size
    - Adjusting the text colors to better match the app theme
    - Adjusting the chart to use app colors instead of default colors
    - Adding a line to separate header text from table content

---
+++ src/app/iso-to-standard-short.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { IsoToStandardShortPipe } from './iso-to-standard-short.pipe';
+
+describe('IsoToStandardShortPipe', () => {
+  it('create an instance', () => {
+    const pipe = new IsoToStandardShortPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
